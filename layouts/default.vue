b59620f224aaea376f7828f4c97ead095d37a290
akam <mr.akam.320@gmail.com>
2023-06-12 12:25:03 +0330

    complete the side bar front-end and back-end

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,7 @@
                     <span class="text-xl w-[9rem]">{{ additionalWork }}</span>
                 </div>
             </div>
-            <div class="w-full h-[20rem] bg-slate-300 rounded-3xl">
+            <div class="w-full h-[20rem] bg-slate-300 rounded-3xl overflow-hidden">
                 <img :src="`http://localhost:1337${photo.data.attributes.url}`" class="w-full h-full object-cover object-top rounded-top" alt="">
             </div>
             <div class="flex flex-col">
@@ -17,11 +17,14 @@
             <div class="">
                 <p class="text-xl text-center text-gray-400">© 2023 Akam All Rights Reserved</p>
             </div>
-            <div class="flex justify-between">
-                <div class="w-20 h-20 rounded-full border">
+            <div class="flex justify-center gap-6">
+                <div v-for="socialMedia in socialMedias" class="w-20 h-20 rounded-full border">
+                    <a :href="socialMedia.link" class="w-full h-full flex justify-center items-center">
+                        <img class="w-10 icon-color" :src="`http://localhost:1337${socialMedia.icon.data.attributes.url}`" alt="">
+                    </a>
                 </div>
             </div>
-            <button class="w-full py-3 rounded-full bg-green-400 text-zinc-900 text-xl">HIRE ME!</button>
+            <a class="w-full py-3 rounded-full bg-green-400 text-zinc-900 text-xl flex justify-center items-center" href="https://t.me/Akam_320">HIRE ME!</a>
         </div>
         <div>
             <slot />
@@ -30,7 +33,13 @@
 </template>
 
 <script setup lang="ts">
-const { data: userInformation }: { data: Ref<any> } = await useFetch("http://localhost:1337/api/sidebar?populate=*")
+const { data: userInformation }: { data: Ref<any> } = await useFetch("http://localhost:1337/api/sidebar?populate[0]=photo&populate[1]=socialMedias.icon")
 
-const { City, email, additionalWork, photo } = userInformation.value.data.attributes
+const { City, email, additionalWork, photo, socialMedias } = userInformation.value.data.attributes
 </script>
+
+<style>
+    .icon-color{
+        filter: invert(93%) sepia(15%) saturate(83%) hue-rotate(177deg) brightness(89%) contrast(95%);
+    }
+</style>
