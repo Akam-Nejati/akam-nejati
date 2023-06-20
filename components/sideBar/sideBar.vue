7d1c8620f055741868dd4213b5c36e0f1cc9e9af
akam <mr.akam.320@gmail.com>
2023-06-20 14:24:35 +0330

    change data of side bar from server to static data

--- components/sideBar/sideBar.vue
+++ components/sideBar/sideBar.vue
@@ -1,32 +1,45 @@
 <script setup lang="ts">
 import thisYear from "~/stores/thisYear"
-const { data: userInformation }: { data: Ref<any> } = await useFetch("http://localhost:1337/api/sidebar?populate[0]=photo&populate[1]=socialMedias.icon")
-const { City, email, additionalWork, photo, socialMedias } = userInformation.value.data.attributes
+import SocialMedia from "~/types/socialMedia.interface";
+import githubIcon from "~/assets/icons/iconmonstr-github-1.svg"
+import telegramIcon from "~/assets/icons/icons8-telegram.svg"
+
+
+const socialMedias: SocialMedia[] = [
+    {
+        icon: githubIcon,
+        link: "https://github.com/Akam-Nejati"
+    },
+    {
+        icon: telegramIcon,
+        link: "https://t.me/Akam_320"
+    }
+]
 </script>
 
 <template>
     <div class="flex justify-between items-top flex-col sm:flex-row md:flex-col md:gap-2 xl:flex-row xl-gap-0">
         <span class="text-2xl xl:text-4xl">Akam Nejati</span>
         <div class="flex flex-col">
-            <span class="text-xl text-gray-400 w-full xl:w-[12rem]">{{ additionalWork }}</span>
+            <span class="text-xl text-gray-400 w-full xl:w-[12rem]">Front-end developer _ vue js</span>
         </div>
     </div>
     <div class="w-full h-[20rem] bg-slate-300 rounded-3xl overflow-hidden">
-        <img :src="`http://localhost:1337${photo.data.attributes.url}`"
+        <img src="~/assets/images/Pixoro-3c3188a68b91c940ae5a4e957548a265.jpg"
             class="w-full h-full object-cover object-center rounded-top" alt="">
     </div>
     <div class="flex flex-col">
-        <span class="text-xl xl:text-3xl">{{ email }}</span>
-        <span class="text-xl xl:text-3xl mt-4">{{ City }}</span>
+        <span class="text-xl xl:text-3xl">mr.akam.320@gmail.com</span>
+        <span class="text-xl xl:text-3xl mt-4">Iran - Mahabad</span>
         <span class="text-xl xl:text-3xl mt-4">born in 2006</span>
     </div>
     <div class="">
         <p class="text-sm xl:text-xl text-center text-gray-400">© {{ thisYear }} Akam All Rights Reserved</p>
     </div>
     <div class="flex justify-center gap-6">
-        <div v-for="(item, index) in socialMedias" :key="index" class="xl:w-20 xl:h-20 rounded-full border">
-            <a :href="item.link" class="w-full h-full flex justify-center items-center">
-                <img class="w-10 icon-color" :src="`http://localhost:1337${item.icon.data.attributes.url}`" alt="">
+        <div v-for="(socialMedia, index) in socialMedias" :key="index" class="xl:w-20 xl:h-20 rounded-full border">
+            <a :href="socialMedia.link" class="w-full h-full flex justify-center items-center">
+                <img class="w-10 icon-color" :src="socialMedia.icon" alt="">
             </a>
         </div>
     </div>
@@ -34,7 +47,7 @@ const { City, email, additionalWork, photo, socialMedias } = userInformation.val
         href="https://t.me/Akam_320">
         <Icon name="line-md:email-twotone" class="text-2xl mr-2" />
         <span class="md:text-sm xl:text-xl">
-            HIRE ME!
+            SEND MASSAGE
         </span>
     </a>
 </template>
---
+++ types/socialMedia.interface.ts
@@ -0,0 +1,4 @@
+export default interface SocialMedia {
+    icon: string,
+    link: string
+}
