86ec24668ffe8dad6616fec47c09bb125ca899a3
Akam Nejati <mr.akam.320@gmail.com>
2023-08-13 14:17:23 +0330

    add gap to side bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-around xl:justify-between">
-        <div class="p-8 m-4 sm:w-[24rem] xl:w-[30rem] rounded-3xl border border-zinc-800 shadow-xl h-[calc(100vh-2rem)] hidden md:flex flex-col justify-between">
+        <div class="p-8 m-4 sm:w-[24rem] xl:w-[30rem] rounded-3xl border border-zinc-800 shadow-xl h-[calc(100vh-2rem)] hidden md:flex flex-col justify-between gap-4">
             <SideBar />
         </div>
         <div class="w-full md:w-[calc(100%-25rem)] xl:w-[calc(100%-31rem)]">
