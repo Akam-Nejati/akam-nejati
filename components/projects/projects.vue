5652209a21c22011ae0b9fe347da701eb1b25797
Akam <mr.akam.320@gmail.com>
2024-06-07 16:46:29 +0330

    change some projects url

--- components/projects/projects.vue
+++ components/projects/projects.vue
@@ -3,6 +3,20 @@ import type Project from '~/types/project.interface';
 
 const projects: Project[] = [
     {
+        openSorce: false,
+        title: "Petro engine",
+        discription: "a shoping website like Divar. (website and dashboard, for admin and users)",
+        preview: "https://code-converter-ai.vercel.app/",
+        github: "https://petro-engine.vercel.app"
+    },
+    {
+        openSorce: false,
+        title: "boomilia",
+        discription: "a architecture web site like Pintrest. (nuxt3)",
+        preview: "https://code-converter-ai.vercel.app/",
+        github: "https://boomilia.com"
+    },
+    {
         title: "Qwitter",
         discription: "a small app same to twitter with vue js and vuetify",
         preview: "https://qwitter.iran.liara.run/home",
@@ -17,20 +31,6 @@ const projects: Project[] = [
     {
         title: "Code converter ai",
         discription: "an app to convert code with chat gpt, vue js and tailwind css",
-        preview: "https://code-converter-ai.vercel.app/",
-        github: "https://github.com/Akam-Nejati/code-converter-ai"
-    },
-    {
-        openSorce: false,
-        title: "Petro engine",
-        discription: "a shoping website like Divar. (website and dashboard, for admin and users)",
-        preview: "https://code-converter-ai.vercel.app/",
-        github: "https://github.com/Akam-Nejati/code-converter-ai"
-    },
-    {
-        openSorce: false,
-        title: "boomilia",
-        discription: "a architecture web site like Pintrest. (nuxt3)",
         preview: "https://code-converter-ai.vercel.app/",
         github: "https://github.com/Akam-Nejati/code-converter-ai"
     },
